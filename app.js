5efe3505f5b1d09f83712ae2b7109ab5c94a4248
Elisa <elisa.gest@hotmail.fr>
2022-03-16 11:40:47 +0100

    ajout code pour lier api et bdd dans app.js

--- app.js
+++ app.js
@@ -7,3 +7,20 @@ app.use((req, res) => {
 });
 
 module.exports = app;
+
+//connexion entre l'api et la bdd  // 
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'dbuser',
+  password : 's3kreee7'
+});
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+  console.log('The solution is: ', rows[0].solution);
+});
+
+connection.end();
