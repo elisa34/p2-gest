8ce948b06ea84cbe776c10a546c2318d0d52f7e3
Elisa <elisa.gest@hotmail.fr>
2022-03-16 11:24:53 +0100

    ajout server.js et modifs app.js

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-const express = require('express')
-const app = express()
-const port = 3000
-app.get('/', (req, res) => {
-    res.send ('Salut')
-})
+const express = require('express') ;
 
-app.listen(port, () => {
-    console.log ('Example app listening on port ${port}')
-})
+const app = express();
+
+app.use((req, res) => {
+    res. json({ message: 'Votre requête a bien êtê regue !' });
+});
+
+module.exports = app;
